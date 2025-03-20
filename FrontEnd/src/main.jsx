c88489b56a0cf8d465e67093a4445182c3ad6d98
Sai Krishna <ivnsainitin@gmail.com>
2025-03-20 13:10:03 +0530

    initial commit

---
+++ FrontEnd/src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, Router, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './Components/Home/Home.jsx'
+import Login from './Components/Login/Login.jsx'
+import Register from './Components/Register/Register.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element = {<Layout />} >
+      <Route path='' element = {<Home />} />
+      <Route path='login' element = {<Login />} />
+      <Route path='register' element = {<Register />} />
+    </Route>
+  )
+)
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
